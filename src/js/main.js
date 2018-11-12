51e970649564913477c37832f809a6c3abc462f8
NoriX <versatility.95@mail.ru>
2018-11-12 21:04:03 +0300

    footer complete

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,25 @@ $(document).ready(function(){
         }
         var slider = $(".slider__list").bxSlider(settings);
     })();
+    ymaps.ready(init);
+    function init(){
+        var myPlacemark = new ymaps.Placemark([55.722314, 37.568605], { hintContent: 'Sharmdeco', balloonContent: 'ул.Ефремова, д.20' });
+        myMap = new ymaps.Map("map", {
+            center: [55.722826, 37.582649],
+            zoom: 14,
+            controls: ['zoomControl']
+        });
+        var searchControl = new ymaps.control.SearchControl({
+             options: {
+                 float: 'left',
+                 floatIndex: 100,
+                 noPlacemark: true,
+                 placeholderContent:"Россия, Москва, ул. Ефремова, 20"
+             }
+        });
+        myMap.controls.add(searchControl);
+        myMap.geoObjects.add(myPlacemark);
+        myMap.behaviors.disable(['drag', 'rightMouseButtonMagnifier', 'scrollZoom'])
+    }
 });
+//
