5130b5b229b0e139dd107a98433f2d2c33cfd117
NoriX <versatility.95@mail.ru>
2018-11-20 23:06:38 +0300

    form in topmenu

--- src/js/form.js
+++ src/js/form.js
@@ -1,34 +1,23 @@
-jQuery(document).ready(function($) {
-	$(".consult__form").submit(function() {
-		var str = $(this).serialize();
-		$.ajax({
-			type: "POST",
-			url: "/wp-content/themes/sharmdeco/mail.php",      // здесь указываем путь ко второму файлу
-			data: str,
-			success: function(msg) {
-				if(msg == 'OK') {
-                    $(".result__message").html("Сообщение отправлено!");
-                    $(".result__message").addClass("success");
-                    $(".form__result").fadeIn(500);
-                    setTimeout(function(){
-                        $(".form__result").fadeOut(500);
-                        $(".result__message").removeClass("success");
-                    }, 5000)
-				}
-				else {
-                    $(".result__message").html(msg);
-                    $(".result__message").addClass("error");
-                    $(".form__result").fadeIn(500);
-                    setTimeout(function(){
-                        $(".form__result").fadeOut(500);
-                        $(".result__message").removeClass("error");
-                    }, 5000)
-                }
-				 $('.form__input', '.consult__form')       // очищаем поля после того, как сообщение отправилось
-                        .not(':button, :submit, :reset, :hidden')
-                        .val('')			 
-			}
-		});
-		return false;
-	});
+$(document).ready(function(){
+    (function(){
+        $(".form__close").on("click", function(e){
+            e.preventDefault();
+            $("#wpcf7-f298-o1").children(".wpcf7-form").removeClass("active");
+            toggleForm();
+        })
+        $(".header__number").on("click", function(e){
+            e.preventDefault();
+            
+            $("#wpcf7-f298-o1").children(".wpcf7-form").toggleClass("active");
+            toggleForm();
+        })
+    })();
+    function toggleForm(){
+        var form = $("#wpcf7-f298-o1").children(".wpcf7-form");
+        if (form.hasClass("active")){
+            form.fadeIn(500);
+        }else{
+            form.fadeOut(500);
+        }
+    }
 });
